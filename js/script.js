52b496dfdf0f15e0f68a4d8e84ab94a9c63f6ae8
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-10 18:27:03 +0100

    secondo almost

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// Mail Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-// Gioco dei dadi Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 // Consigli del giorno:
 // scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
@@ -9,6 +7,8 @@
 // proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 
 // PRIMO ESERCIZIO 
+// Mail Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+//  stampa un messaggio appropriato sull’esito del controllo.
 
 
 // ARRAY DELL'EMAIL
@@ -19,11 +19,11 @@ email.push('carla.@gmail.com','paola.@gmail.com');
 let x = email.length;
 console.log(x);
 console.log(email);
-// chiedo all'utente la sua email 
+// PROMPT EMAIL DELL'UTENTE
 let emailutente = prompt("Qual'è la tua email?");
 // CONTROLLO ARRAY ATTRAVERSO CICLO
 let sentinella = false;
-for(let i = 0;i < email.length; i++){
+for(let i = 0; i < email.length; i++){
  console.log(email[i]);
  // CONDIZIONE SE LA SENTINELLA IS TRUE
  if (email[i] == emailutente) {
@@ -39,3 +39,17 @@ console.log("email non trovata");
 
 
 // SECONDO ESERCIZIO 
+// Gioco dei dadi Generare un numero random da 1 a 6,
+//  sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// GENERARE I NUMERI CON ARRAY E CREARE UN RANDOM DEI NUMERI, STABILIRE IL VINCITORE
+
+  let random = Math.floor(Math.random()*6)+ 1;
+   console.log(random);
+  let randomCPU = parseInt(prompt("Please enter your number"));
+
+
+  document.getElementById("random").addEventListener("click", myFunction);
+    function myFunction() {
+      alert ("Hello World!");
+} 
